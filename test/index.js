cc170a1cbcd7dcc3cdc4bad1f0d0f8bffd6acc03
Matt Simerson <matt@tnpi.net>
2026-03-09 23:40:09 -0700

    chore: remove unneeded done callbacks

--- test/index.js
+++ test/index.js
@@ -4,50 +4,45 @@ const assert = require('assert')
 // npm modules
 const fixtures = require('haraka-test-fixtures')
 
-beforeEach((done) => {
+beforeEach(() => {
   this.plugin = new fixtures.plugin('dcc')
-  done() // if a test hangs, assure you called done()
 })
 
 describe('dcc', () => {
-  it('loads', (done) => {
+  it('loads', () => {
     assert.ok(this.plugin)
-    done()
   })
 })
 
 describe('load_dcc_ini', () => {
-  beforeEach((done) => {
+  beforeEach(() => {
     this.plugin = new fixtures.plugin('dcc')
     this.plugin.load_dcc_ini()
-    done()
   })
 
-  it('loads dcc.ini from config/dcc.ini', (done) => {
+  it('loads dcc.ini from config/dcc.ini', () => {
     assert.ok(this.plugin.cfg.main)
-    done()
   })
 
-  it('detects set path', (done) => {
+  it('detects set path', () => {
     assert.equal(
       this.plugin.cfg.dccifd.path,
       '/var/dcc/dccifd',
       this.plugin.cfg,
     )
-    done()
+
   })
 })
 
 describe('get_host', () => {
   ;['Unknown', 'NXDOMAIN', 'DNSERROR', undefined].forEach((e) => {
-    it(`returns undefined for ${e}`, (done) => {
+    it(`returns undefined for ${e}`, () => {
       assert.equal(this.plugin.get_host(e), undefined)
-      done()
     })
   })
 
-  it('returns a hostname', (done) => {
+  it('returns a hostname', () => {
     assert.equal(this.plugin.get_host('host'), 'host')
-    done()
+
   })
 })
